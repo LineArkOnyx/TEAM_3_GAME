93414815ef9c2aec502d9e6f965f1352895f5a95
oisiikani <144640142+oisiikani@users.noreply.github.com>
2025-07-02 12:34:45 +0900

    セレクトシーン

--- src/Scene/Select/SelectScene.cpp
+++ src/Scene/Select/SelectScene.cpp
@@ -1,18 +1,114 @@
 #include "SelectScene.h"
 
+#define PLAYER_SPEED (10)
+#define GRAY_COLOR  (125,125,125)
+
 void SelectScene::Init()
 {
+
+	PlayerPosX = 0.0f;
+	PlayerPosY = 0.0f;
+	CurrentStage = 0;
+	DecisionStage = 0;
+	NextStage = 0;
+	m_IsOverX = false;
+	m_IsOverY = false;
+	m_IsMove = false;
+
 
 }
 void SelectScene::Load()
 {
 
+	PlayerPosX = m_eStageData[CurrentStage].x;
+	PlayerPosY = m_eStageData[CurrentStage].y;
+
+	NextStage = -1;
+
+	StageIconHandle = LoadGraph(ISLAND_HANDLE);
+	PlayerIconHandle = LoadGraph(PLAYER_HANDLE);
+
+
 }
 int SelectScene::Step()
 {
+
+	float kakudo;
+
+	//kakudo = atan2f((m_eStageData[1].x - m_eStageData[0].x), (m_eStageData[1].y - m_eStageData[0].y));
+	kakudo = atan2f((m_eStageData[NextStage].x - m_eStageData[CurrentStage].x),
+		(m_eStageData[NextStage].y - m_eStageData[CurrentStage].y));
+
+
+	if (CInput::IsKeyPush(KEY_INPUT_RIGHT) && !m_IsMove)
+	{
+		if (m_eStageData[CurrentStage].AfterStage != -1)
+		{
+			NextStage = m_eStageData[CurrentStage].AfterStage;
+			m_IsMove = true;
+
+			if (m_eStageData[CurrentStage].x < m_eStageData[CurrentStage + 1].x)
+				m_IsOverX = true;
+			else m_IsOverX = false;
+		}
+		else;
+
+	}
+	if (CInput::IsKeyPush(KEY_INPUT_LEFT) && !m_IsMove)
+	{
+		if (m_eStageData[CurrentStage].BeforeStage != -1)
+		{
+			NextStage = m_eStageData[CurrentStage].BeforeStage;
+			m_IsMove = true;
+
+			if (m_eStageData[CurrentStage].x < m_eStageData[CurrentStage - 1].x)
+				m_IsOverX = true;
+			else m_IsOverX = false;
+		}
+		else;
+	}
+
+	if (m_IsMove)
+	{
+		PlayerPosX += sinf(kakudo) * PLAYER_SPEED;
+		PlayerPosY += cosf(kakudo) * PLAYER_SPEED;
+
+		if (m_IsOverX)
+		{
+			if (m_eStageData[NextStage].x < PlayerPosX)
+			{
+				PlayerPosX = m_eStageData[NextStage].x;
+				PlayerPosY = m_eStageData[NextStage].y;
+
+				m_IsMove = false;
+				CurrentStage = NextStage;
+				NextStage = -1;
+
+			}
+		}
+		else
+		{
+			if (m_eStageData[NextStage].x > PlayerPosX)
+			{
+
+				PlayerPosX = m_eStageData[NextStage].x;
+				PlayerPosY = m_eStageData[NextStage].y;
+
+				m_IsMove = false;
+				CurrentStage = NextStage;
+				NextStage = -1;
+
+			}
+		}
+
+	}
+
 	int Sequence = 0;
-	if (CInput::IsKeyPush(KEY_INPUT_RETURN)) {
+
+	if (CInput::IsKeyPush(KEY_INPUT_RETURN) && !m_IsMove) {
 		Sequence = 1;
+		DecisionStage = CurrentStage;
+
 	}
 	return Sequence;
 }
@@ -21,14 +117,14 @@ void SelectScene::Exit()
 
 }
 
-SelectScene::SelectScene()
-{
-
-}
-SelectScene::~SelectScene()
-{
-
-}
+//SelectScene::SelectScene()
+//{
+//
+//}
+//SelectScene::~SelectScene()
+//{
+//
+//}
 
 int SelectScene::Loop()
 {
@@ -70,7 +166,25 @@ int SelectScene::Loop()
 }
 void SelectScene::Draw()
 {
-	switch (SequenceID)
+
+	DrawBox(0, 0, 1280, 720, GetColor(50, 70, 225), true);
+
+	for (int Index = 0; Index < STAGE_MAX; Index++)
+	{
+		if (Index < STAGE_MAX - 1)
+		{
+			DrawLineAA(m_eStageData[Index].x, m_eStageData[Index].y,
+				m_eStageData[Index + 1].x, m_eStageData[Index + 1].y, GRAY_COLOR);    // ü‚ð•`‰æ
+		}
+
+		DrawRotaGraph((int)m_eStageData[Index].x, (int)m_eStageData[Index].y, 1.0f, 0.0f, StageIconHandle, true);
+
+
+	}
+
+	DrawRotaGraph((int)PlayerPosX, (int)PlayerPosY, 1.0f, 0.0f, PlayerIconHandle, true);
+
+	/*switch (SequenceID)
 	{
 	case INIT_SEQUENCE:
 		DrawFormatString(0, 0, GetColor(255, 255, 0), "SELECT_INIT");
@@ -90,5 +204,5 @@ void SelectScene::Draw()
 
 	default:
 		break;
-	}
+	}*/
 }
--- src/Scene/Select/SelectScene.h
+++ src/Scene/Select/SelectScene.h
@@ -2,6 +2,16 @@
 
 #include "DxLib.h"
 #include "../../Input/Input.h"
+#include "math.h"
+
+#define	SCREEN_SIZE_X	1280	// X方向の画面サイズを指定(デフォルト)
+#define	SCREEN_SIZE_Y	720	// Y方向の画面サイズを指定(デフォルト)
+
+#define STAGE_MAX	(5)
+
+#define PLAYER_HANDLE ("data/Select/kari.png")
+#define ISLAND_HANDLE ("data/Select/island.png")
+
 class SelectScene
 {
 private:
@@ -15,6 +25,28 @@ private:
 		SEQUENCE_NUM,
 	};
 
+	struct STAGE_DATA
+	{
+		float x;
+		float y;
+
+		int id;
+
+		int BeforeStage;
+		int AfterStage;
+	};
+
+	const STAGE_DATA m_eStageData[STAGE_MAX] =
+	{
+
+		{100.0f,300.0f,0,-1,1},
+		{300.0f,200.0f,1,0,2},
+		{500.0f,500.0f,2,1,3},
+		{700.0f,400.0f,3,2,4},
+		{900.0f,100.0f,4,3,-1},
+
+	};
+
 	SEQUENCE_ID SequenceID = INIT_SEQUENCE;
 
 	void Init();
@@ -22,9 +54,24 @@ private:
 	int Step();
 	void Exit();
 
+	float PlayerPosX;
+	float PlayerPosY;
+
+	int CurrentStage;
+	int NextStage;
+	int DecisionStage;
+
+	bool m_IsMove;
+
+	bool m_IsOverX;
+	bool m_IsOverY;
+
+	int StageIconHandle;
+	int PlayerIconHandle;
+
 public:
-	SelectScene();
-	~SelectScene();
+	SelectScene() = default;
+	~SelectScene() = default;
 
 	int Loop();
 	void Draw();
